6f6ad4432d11a190b6c5894a00b2ec445fa012a4
GitHub Action <action@github.com>
2025-04-08 08:52:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/tuanphung2308/ideacopilot/MyBundle.kt
+++ src/main/kotlin/com/github/tuanphung2308/ideacopilot/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.tuanphung2308.ideacopilot
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/tuanphung2308/ideacopilot/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/tuanphung2308/ideacopilot/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.tuanphung2308.ideacopilot.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
